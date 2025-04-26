7fc79fea468f0d728979434dbf99d8b974a96f3f
jimfoxes <jimfoxes21@yandex.ru>
2025-04-26 11:34:52 +0500

    Настройка окружения

---
+++ eslint.config.js
@@ -0,0 +1,36 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import prettier from 'eslint-plugin-prettier/recommended'
+
+export default [
+    { ignores: ['dist'] },
+    prettier, // Добавляем конфигурацию Prettier (включает и plugin и config-prettier)
+    {
+        files: ['**/*.{js,jsx}'],
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.browser,
+            parserOptions: {
+                ecmaVersion: 'latest',
+                ecmaFeatures: { jsx: true },
+                sourceType: 'module',
+            },
+        },
+        plugins: {
+            'react-hooks': reactHooks,
+            'react-refresh': reactRefresh,
+        },
+        rules: {
+            ...js.configs.recommended.rules,
+            ...reactHooks.configs.recommended.rules,
+            'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+            'react-refresh/only-export-components': [
+                'warn',
+                { allowConstantExport: true },
+            ],
+            'prettier/prettier': ['error', {}, { usePrettierrc: true }], // Читает настройки из .prettierrc
+        },
+    },
+]
