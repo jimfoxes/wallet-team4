3da514690d7ce1c47fe3b1384f577da6c032f5a5
jimfoxes <32494631+jimfoxes@users.noreply.github.com>
2025-04-29 01:24:14 +0500

    Revert "first-meeting"

--- src/pages/SignIn.jsx
+++
@@ -1,7 +0,0 @@
-import AuthorisationForm from "../components/AuthorisationForm/AuthorisationForm";
-
-const SignInPage = () => {
-  return <AuthorisationForm isSignUp={false} />;
-};
-
-export default SignInPage;
--- src/pages/SignUp.jsx
+++
@@ -1,7 +0,0 @@
-import AuthorisationForm from "../components/AuthorisationForm/AuthorisationForm";
-
-const SignUpPage = () => {
-  return <AuthorisationForm isSignUp />;
-};
-
-export default SignUpPage;
